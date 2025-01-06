7a148bd66249d0128f10f1584044e3b0d099fea6
nasim0994 <nasimuddin0994@gmail.com>
2025-01-06 16:01:34 +0600

    update nextjs config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
 const nextConfig = {
   images: {
-    domains: ["api.saifullanju.com", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.saifullanju.com",
+        pathname: "/**",
+      },
+    ],
   },
   env: {
     PORT: process.env.PORT || 2500,
